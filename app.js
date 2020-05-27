54db41aa2b394dc5cb32325d0d63a879293d9cd4
Rachit <rvadhvaryu@gmail.com>
2020-05-27 13:49:16 +0530

    Submitting Ass 2

---
+++ app.js
@@ -0,0 +1,50 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuy = this;
+  toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+  toBuy.buy = function(index) {
+    ShoppingListCheckOffService.buyItem(index);
+  };
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var bought = this;
+  bought.items = ShoppingListCheckOffService.getBoughtItems();
+
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var toBuyItems = [
+    { name: "Cheese", quantity: 2 },
+    { name: "Orange", quantity: 15 },
+    { name: "Potato Chips", quantity: 5 },
+    { name: "Milk Powder", quantity: 10 },
+    { name: "Cold Drinks", quantity: 20 }];
+
+  var boughtItems = [];
+
+  service.buyItem = function(index) {
+    boughtItems.push(toBuyItems[index]);
+    toBuyItems.splice(index, 1);
+  };
+
+  service.getToBuyItems = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+}
+
+})();
